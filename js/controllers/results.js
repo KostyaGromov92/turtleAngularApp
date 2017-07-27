51e24d8830b5545630758bfa58fc5e6990611ca7
Kostya Gromov <konstantin.gromov@swanlogic.com>
2017-07-27 12:11:42 +0300

    Add final part for this app

--- js/controllers/results.js
+++ js/controllers/results.js
@@ -12,7 +12,39 @@
 
         vm.quizMetrics = quizMetrics;
         vm.dataService = DataService;
+        vm.activeQuestion = 0;
+        vm.setActiveQuestion = setActiveQuestion;
+        vm.getAnswerClass = getAnswerClass;
+        vm.calculatePerc = calculatePerc;
+        vm.reset = reset;
 
+        function  reset () {
+            quizMetrics.changeState("results", false);
+            quizMetrics.numCorrect = 0;
+
+            for (var i = 0; i < DataService.quizQuestions.length; i++) {
+                var data = DataService.quizQuestions[i];
+
+                data.selected = null;
+                data.correct = null;
+            }
+        }
+
+        function calculatePerc () {
+            return quizMetrics.numCorrect / DataService.quizQuestions.length * 100;
+        }
+
+        function setActiveQuestion (index) {
+            vm.activeQuestion = index;
+        }
+
+        function  getAnswerClass (index) {
+            if (index === quizMetrics.correctAnswers[vm.activeQuestion]) {
+                return "bg-success";
+            } else if (index === DataService.quizQuestions[vm.activeQuestion].selected) {
+                return "bg-danger";
+            }
+        }
     }
     
 })();
